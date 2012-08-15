5f87ed0b20c328b7e27265132e56e96f8137f725
DAZ <daz4126@gmail.com>
2012-08-15 19:49:17 +0100

    database added

--- main.rb
+++ main.rb
@@ -1,12 +1,40 @@
 require 'sinatra'
+require 'dm-core'
+require 'dm-migrations'
+require 'dm-timestamps'
 require 'sass'
 require 'slim'
+
+configure do
+  DataMapper.setup(:default, ENV['DATABASE_URL'] || File.join("sqlite3://",settings.root, "development.db"))
+end
+
+class Riddle
+  include DataMapper::Resource
+  property    :id,           Serial
+
+  property :created_at, DateTime
+  property :updated_at, DateTime
+
+  property :title,           String
+
+  property    :html,         Text
+  property    :css,          Text
+  property    :js,           Text
+
+  def title=(value) 
+    super(value.empty? ? "Yet another untitled Riddle" : value) 
+  end 
+end
+
+DataMapper.finalize
 
 get '/css/styles.css' do
   scss :styles
 end
 
 get '/' do
+  @riddles = Riddle.all.reverse
   slim :index
 end
 
@@ -14,8 +42,14 @@ get '/new/riddle' do
   slim :new
 end
 
-post '/riddle' do
+get '/riddle/:id' do
+  @riddle = Riddle.get(params[:id])
   slim :show
+end
+
+post '/riddle' do
+  riddle = Riddle.create(params[:riddle])
+  redirect to("/riddle/#{riddle.id}")
 end
 
 __END__
@@ -36,26 +70,35 @@ html lang="en"
       == yield
 
 @@index
-h1 Index Page
-p This will list all of the riddles
+- if @riddles
+  - @riddles.each do |riddle|
+    li
+      a href="/riddle/#{riddle.id}"== riddle.title
+- else
+  p No riddles have been created yet!
 
 @@new
 == slim :form
 
 @@show
-h1 Riddle Show page
-p This will show the riddle
+h1== @riddle.title
+h2 Markup
+p== @riddle.html
+h2 Styles
+p== @riddle.css
+h2 Javascript
+p== @riddle.js
 
 @@form
 form action="/riddle" method="POST"
   label for="title" Title
   input#title name="riddle[title]"
   label for="html" HTML
-  textarea#html cols=60 rows=10
+  textarea#html cols=60 rows=10 name="riddle[html]"
   label for="css" CSS
-  textarea#css cols=60 rows=10
+  textarea#css cols=60 rows=10 name="riddle[css]"
   label for="js" JS
-  textarea#js cols=60 rows=10
+  textarea#js cols=60 rows=10 name="riddle[js]"
   input.button type="submit" value="Save"
 
 @@styles
